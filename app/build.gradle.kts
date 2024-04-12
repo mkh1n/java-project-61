a59bab4e582bff716e1f2864341ea7fd33494376
Roman <romamuhino1@gmai.com>
2024-04-12 14:48:14 +0300

    add greating

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -8,9 +9,12 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+application {
 
+    mainClass = "hexlet.code.App"
+}
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,8 @@ package hexlet.code;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class app {
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        System.out.println("Welcome to the Brain Games!");
     }
 }
