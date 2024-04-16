571b3d2179e32d5cf371e4051404bbb5966042dd
Roman <romamuhino1@gmai.com>
2024-04-17 00:24:50 +0300

    fix problem with prime game

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -5,6 +5,17 @@ import java.math.BigInteger;
 public class Prime {
     @SuppressWarnings("checkstyle:<MagicNumber>")
 
+    public static boolean isPrime(Integer number) {
+        if(number < 2) return false;
+        for(int i = 2; i < number / 2; i++) {
+            if(number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    @SuppressWarnings("checkstyle:<MagicNumber>")
+
     public static String[][] main() {
         String[] rules = new String[]{"Answer 'yes' if given number is prime. Otherwise answer 'no'."};
         String[] questions = new String[3];
@@ -12,9 +23,7 @@ public class Prime {
         for (int i = 0; i < 3; i += 1) {
             var randomNumber = (int) Math.floor(Math.random() * 100);
             questions[i] = Integer.toString(randomNumber);
-            BigInteger bigInteger = BigInteger.valueOf(randomNumber);
-            boolean probablePrime = bigInteger.isProbablePrime((int) Math.log(randomNumber));
-            rightAnswers[i] = probablePrime ? "yes" : "no";
+            rightAnswers[i] = isPrime(randomNumber) ? "yes" : "no";
         }
         return new String[][]{rules, questions, rightAnswers};
     }
