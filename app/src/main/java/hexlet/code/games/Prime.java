3ae0716db676600c4498ac3b2c22753e8b4d68e8
Roman <romamuhino1@gmai.com>
2024-09-25 02:38:08 +0300

    fix checkstyle problems

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -11,11 +11,11 @@ public final class Prime implements Game {
     }
     private static int number;
 
-    public static boolean isPrime(Integer num) {
+    private static boolean isPrime(int num) {
         if (num < 2) {
             return false;
         }
-        for (int i = 2; i < num / 2; i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
