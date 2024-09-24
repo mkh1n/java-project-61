4dc142c81641e76639b8a68983c907ae07bf3d25
Roman <romamuhino1@gmai.com>
2024-09-25 02:27:14 +0300

    fix checkstyle problems

--- app/src/main/java/hexlet/code/Rand.java
+++ app/src/main/java/hexlet/code/Rand.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class Rand {
-    private final static int HUNDRED = 100;
+    final private static int HUNDRED = 100;
     public static int getRandomNumber() {
         return (int) Math.floor(Math.random() * HUNDRED);
     }
